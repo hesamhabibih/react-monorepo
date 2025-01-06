af0629fdfc90ec13240e0acfa5048642aa4d5d80
Habibi, Hesam <he.habibi@asiatech.ir>
2025-01-06 11:27:25 +0330

    add

--- apps/react-monorepo/jest.config.ts
+++ apps/react-monorepo/jest.config.ts
@@ -1,6 +1,7 @@
 export default {
   displayName: 'react-monorepo',
   preset: '../../jest.preset.js',
+   testEnvironment: 'node',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
